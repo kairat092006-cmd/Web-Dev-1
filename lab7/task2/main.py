5aaa305820ee5bccf29eb67aedf776bca7260942
kairat092006-cmd <kairat092006@gmail.com>
2026-03-19 09:00:52 +0500

    main.py

--- lab7/task2/main.py
+++ lab7/task2/main.py
@@ -2,8 +2,8 @@ from models import Animal, Dog, Cat
 
 animals = [
     Animal("Animal", 5, "gray"),
-    Dog("Rex", 3, "brown"),
-    Cat("Milo", 2, "white")
+    Dog("Garry", 3, "black/white"),
+    Cat("Maxwel", 2, "orange")
 ]
 
 for animal in animals:
